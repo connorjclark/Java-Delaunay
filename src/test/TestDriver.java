32112d68be0049f7588391fb8567f5e51b71bf43
Hoten <cjamcl@gmail.com>
2013-08-01 21:20:57 -0500

    moved test driver to more appropriate package

--- src/test/TestDriver.java
+++ src/test/TestDriver.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package hoten.voronoi.test;
+package test;
 
 import hoten.geom.Point;
 import hoten.geom.Rectangle;
