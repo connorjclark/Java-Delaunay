57e5b6e9b1236dc59aca8d26e3ea832b3d5031d5
Connor Clark <cjamcl@gmail.com>
2014-12-03 18:00:53 -0600

    createMap in TestDriver

--- src/test/java/hoten/delaunay/TestDriver.java
+++ src/test/java/hoten/delaunay/TestDriver.java
@@ -15,25 +15,12 @@ import javax.swing.WindowConstants;
 public class TestDriver {
 
     public static void main(String[] args) throws IOException {
-        final int width = 2000;
-        final int height = 2000;
-        final int numSites = 10000;
-        final long seed = System.nanoTime();
-        final Random r = new Random(seed);
+        int bounds = 2000;
+        int numSites = 10000;
+        long seed = System.nanoTime();
         System.out.println("seed: " + seed);
 
-        //make the intial underlying voronoi structure
-        final Voronoi v = new Voronoi(numSites, width, height, r, null);
-
-        //assemble the voronoi strucutre into a usable graph object representing a map
-        final TestGraphImpl graph = new TestGraphImpl(v, 2, r);
-
-        final BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
-        Graphics2D g = img.createGraphics();
-        g.setColor(Color.BLACK);
-        g.fillRect(0, 0, width, height);
-
-        graph.paint(g);
+        final BufferedImage img = createMap(bounds, numSites, seed);
 
         ImageIO.write(img, "PNG", new File(String.format("%s.png", seed)));
 
@@ -46,7 +33,26 @@ public class TestDriver {
 
         frame.setTitle("java fortune");
         frame.setVisible(true);
-        frame.setSize(width + 50, height + 50);
+        frame.setSize(img.getWidth() + 50, img.getHeight() + 50);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public static BufferedImage createMap(int bounds, int numSites, long seed) {
+        final Random r = new Random(seed);
+
+        //make the intial underlying voronoi structure
+        final Voronoi v = new Voronoi(numSites, bounds, bounds, r, null);
+
+        //assemble the voronoi strucutre into a usable graph object representing a map
+        final TestGraphImpl graph = new TestGraphImpl(v, 2, r);
+
+        final BufferedImage img = new BufferedImage(bounds, bounds, BufferedImage.TYPE_4BYTE_ABGR);
+        Graphics2D g = img.createGraphics();
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, bounds, bounds);
+
+        graph.paint(g);
+
+        return img;
     }
 }
