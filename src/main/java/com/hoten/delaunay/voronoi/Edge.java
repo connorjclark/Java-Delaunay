fc2307973c3d2e0ead4fbe85aa6db03a6e964038
Connor Clark <cjamcl@gmail.com>
2015-03-06 10:43:46 -0600

    proper package structure, v0.1

---
+++ src/main/java/com/hoten/delaunay/voronoi/Edge.java
@@ -0,0 +1,23 @@
+package com.hoten.delaunay.voronoi;
+
+import com.hoten.delaunay.geom.Point;
+
+/**
+ * Edge.java
+ *
+ * @author Connor
+ */
+public class Edge {
+
+    public int index;
+    public Center d0, d1;  // Delaunay edge
+    public Corner v0, v1;  // Voronoi edge
+    public Point midpoint;  // halfway between v0,v1
+    public int river;
+
+    public void setVornoi(Corner v0, Corner v1) {
+        this.v0 = v0;
+        this.v1 = v1;
+        midpoint = new Point((v0.loc.x + v1.loc.x) / 2, (v0.loc.y + v1.loc.y) / 2);
+    }
+}
---
+++ src/main/java/com/hoten/delaunay/voronoi/nodename/as3delaunay/Winding.java
@@ -0,0 +1,19 @@
+package com.hoten.delaunay.voronoi.nodename.as3delaunay;
+
+public final class Winding {
+
+    final public static Winding CLOCKWISE = new Winding("clockwise");
+    final public static Winding COUNTERCLOCKWISE = new Winding("counterclockwise");
+    final public static Winding NONE = new Winding("none");
+    private String _name;
+
+    private Winding(String name) {
+        super();
+        _name = name;
+    }
+
+    @Override
+    public String toString() {
+        return _name;
+    }
+}
